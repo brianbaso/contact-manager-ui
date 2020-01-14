355e986de1e4c00dab04a4b014340491df5c5dd5
Brian <brianbaso@gmail.com>
2020-01-14 18:42:14 -0500

    firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXZKI_f6zs14h121Mno6aWbG0ZjkflcjM",
+  authDomain: "contact-manager-98599.firebaseapp.com",
+  databaseURL: "https://contact-manager-98599.firebaseio.com",
+  projectId: "contact-manager-98599",
+  storageBucket: "contact-manager-98599.appspot.com",
+  messagingSenderId: "470819990763",
+  appId: "1:470819990763:web:50e696ba364eabcd37a2ae",
+  measurementId: "G-4XJFQ7XM3R"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
