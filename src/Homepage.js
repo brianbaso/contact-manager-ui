c901e9ef4ac092d9c68fd21286279e50e7761087
Sarah71144 <54606672+Sarah71144@users.noreply.github.com>
2020-01-25 15:53:39 -0500

    homepage progress

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from "./Header.js";
 import ContactCard from "./contactCard.js";
 import "./HomepageStyle.scss";
-// need to resolve the file for axios or something
 import axios from 'axios';
 
 //import UsersContactCard from "./UsersContactCard.js";
@@ -10,10 +9,6 @@ import axios from 'axios';
 //import ContactList from "./ContactList.js";
 
 class Homepage extends React.Component {
-    // constructor with info saved in this.state ={}
-    // component did mount once page loads run
-    //contacts.forEach
-
     constructor(props) {
         super(props);
 
@@ -25,7 +20,7 @@ class Homepage extends React.Component {
     componentDidMount() {
         // temporary contacts array
         let contacts = [];
-        // this is the url for get from postman... it works in post so it should work here? or no?
+        // cors-anywhere to fix the cors error
         axios.get('https://cors-anywhere.herokuapp.com/https://us-central1-contact-manager-98599.cloudfunctions.net/webAPI/api/v1/contacts')
             .then((contacts) => {
                 this.setState({ contacts: contacts.data });
@@ -35,41 +30,86 @@ class Homepage extends React.Component {
             });
     }
 
-    render()
-    {
+    render() {
         // javascript
-        // temporary contacts
+        // this will have all the contacts in it
         const contacts = [];
 
         this.state.contacts.forEach((contact) => {
             // optional console log to see data the JSON package
             console.log('contact', contact);
             contacts.push(
-              // TODO: ContactCard will accept props
+                // TODO: ContactCard will accept props
                 <ContactCard
                     key={contact.id}
                     id={contact.id}
                     name={contact.data.name}
-                    phone={contact.data.phoneNumber}
+                    phoneNumber={contact.data.phoneNumber}
                     address={contact.data.address}
                 />
             );
+
         });
         console.log(contacts);
-
+        
+        
         return (
             // search bar after the header
-            // removed... <h1> Contacts </h1>
-            // removed... <ContactCard />
             <div>
                 <Header />
-                {contacts}
+                {contacts.map((contact, index) => { return <ContactCard name={contact.name} phoneNumber={contact.phoneNumber} address={contact.address} /> })}
+
 
             </div>
-                // I have to add the list in here somewhere..
-
-            );
+            
+        );
     }
+
+/* BEST ATTEMPT SO FAR WAS UNDER HEADER
+           {contacts.map((contact, index) => {
+               //console.log(contact)
+               return <ContactCard {...contact}/>
+           })}
+           */
+            // I have to add the list in here somewhere..
+            // {contacts} (was under <Header />)
+            // removed... <h1> Contacts </h1>
+            // !removed... <ContactCard /> under header
+            // pass in the props to the contact card component
+            // passed in the array of contactCard objects into contactCard.js
+/*
+    {
+        contacts.map((contact, index) =>
+            <ContactCard
+                name={contact.data.name}
+                phoneNumber={contact.data.phoneNumber}
+                address={contact.data.address}
+            />
+        )
+    }
+    */
+/*
+var i;
+for (i = 0; i < contacts.length; i++)
+{
+    <ContactCard
+        name={contacts[i].data.name}
+        phoneNumber={contacts[i].data.phoneNumber}
+        address={contacts[i].data.address}
+    />
+}
+*/
+        // youtube tut
+
+/*
+contacts.forEach(function (contact) {
+    <ContactCard
+        name={contact.data.name}
+        phoneNumber={contact.data.phoneNumber}
+        address={contact.data.address}
+    />
+})
+*/
 }
 
 export default Homepage;
--- src/contactCard.js
+++ src/contactCard.js
@@ -1,21 +1,54 @@
 ﻿import React from 'react';
+import { Component } from 'react';
 import "./ContactCardStyle.scss";
 
+// this will accept props
 class ContactCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillRender() {
+        console.log(this.props)
+    }
 
     render() {
         return (
-            
             <div4>
-                <name1> Name: </name1>
-                <phone1> Phone: </phone1>
-                <ad1> Address: </ad1>
-            </div4>
-
+                <p>
+                    <name1> Name: {this.props.name} </name1> <br /> <br />
+                    <phone1> Phone: {this.props.phoneNumber} </phone1> <br /> <br />
+                    <ad1> Address: {this.props.address}  </ad1> <br /> <br />
+                </p>
+            </div4>    
         );
-
-
     }
+
+
+/*
+processProps = () => {
+   
+    return { contactList };
+}
+this.props.map((this.props, index) =>
+            <p key={index}>
+*/
+/*
+ <p>
+                {this.props.data.name} <br />
+                {this.props.data.phoneNumber} <br />
+                {this.props.data.address}
+  </p>
+ */
+
+
+/*
+        <div4>
+            <name1> Name: {this.props.data.} </name1>
+            <phone1> Phone: {this.props.data.phoneNumber}</phone1>
+            <ad1> Address: {fields.data.address}</ad1>
+        </div4>
+        */
+
 }
 
 
