e0aca38beac24d9c66cd92b5b3d4325c5096d907
joseiciano <icianojose12345@aol.com>
2020-01-30 19:24:34 -0500

    added page for logout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Redirect } from "react-router";
 import PrivateRoute from "./components/auth/PrivateRoute";
 import { AuthProvider } from "./components/auth/Auth";
+import Logout from "./components/auth/Logout";
+import { AuthContext } from "./components/auth/Auth";
 
 // Change '/' and '/profile' to privateroutes after auth is allowed
 // Only '/authentication' should be public
 
+const HomePageTest = () => {
+  // const { currentUser } = useContext(AuthContext);
+  // if (currentUser) {
+  //   return <Redirect to="/login" />;
+  // }
+
+  const { currentUser } = useContext(AuthContext);
+  if (!currentUser) {
+    return <Redirect to="/login" />;
+  }
+
+  return (
+    <div>
+      <Logout />
+    </div>
+  );
+};
+
 const App = () => (
   <AuthProvider>
     <Router>
-      {/* <Route exact path="/" component={HomePage} /> */}
-      <Route exact path="/" component={LoginPage} />
+      <PrivateRoute exact path="/" component={HomePageTest} />
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={RegisterPage} />
-      {/* <Route exact path="/profile" component={ProfilePage} /> */}
     </Router>
   </AuthProvider>
 );
