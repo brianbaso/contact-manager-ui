fc819e9b157632812ed85c828f961395c9e75638
dustinct1996 <dustin.ct1996@knights.ucf.edu>
2020-01-30 19:52:02 -0500

    connected create contact button to database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import CreateContactPage from "./pages/CreateContactPage.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router>
+          <Route exact path="/CreateContactPage" component={CreateContactPage} />
+        </Router>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Contact Manager UI
