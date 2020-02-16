538c14b5b31ddfed7c627b3b523151b389368d78
Sarah71144 <54606672+Sarah71144@users.noreply.github.com>
2020-02-16 14:34:34 -0500

    changed the header in the create a contact, even though I can't see the create contact button to get to it...

--- src/components/general/CCHeader.js
+++ src/components/general/CCHeader.js
@@ -6,7 +6,7 @@ class CCHeader extends React.Component {
     return (
       <div style={styles.div}>
         <Logout></Logout>
-        <t1 style={styles.t1}>Contact Manager</t1>
+        <t1 style={styles.t1}>Contactly</t1>
       </div>
     );
   }
